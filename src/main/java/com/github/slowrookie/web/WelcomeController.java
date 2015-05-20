6df5e206da51c5627cd63ac8f8da15c216a2465e
slowrookie <liujiaxingemail@gmail.com>
2015-05-21 00:14:46 +0800

    hello

---
+++ src/main/java/com/github/slowrookie/web/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.github.slowrookie.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@RequestMapping("/hello")
+	public Map<String, String> hello() {
+		Map<String, String> result = new HashMap<>();
+		result.put("hello", "world!");
+		return result;
+	}
+	
+}
